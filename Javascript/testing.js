5b09606f5a51cbd4637fee87b8f53570be205dde
n3utr1n0 <anuragpalofficial@gmail.com>
2020-08-12 14:49:23 +0530

    Contribute to this folder

---
+++ Javascript/testing.js
@@ -0,0 +1,16 @@
+Qual.sw("Welcome","Nice to meet you",succ,"Submit","Close","hello","bu","text","TEXT");
+
+
+
+// Qual.sdh("hello","Help")
+var a, b;
+// popalert.simple("Hello World")
+function hello() {
+  a = inx;
+  console.log(a);
+}
+
+function max() {
+  b = inx;
+  console.log(b);
+}
